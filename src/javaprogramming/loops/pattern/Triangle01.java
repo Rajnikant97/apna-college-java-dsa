b7db674bc2ffa27087f11712e018e295da77683b
Rajanikant <kantrajni304@gmail.com>
2022-08-28 10:31:01 +0530

    loops - triangle01 pattern second method

--- src/javaprogramming/loops/pattern/Triangle01.java
+++ src/javaprogramming/loops/pattern/Triangle01.java
@@ -3,10 +3,24 @@ package javaprogramming.loops.pattern;
 public class Triangle01 {
     public static void main(String[] args) {
         Triangle01 t = new Triangle01();
-        t.print(5);
+        t.printMethod1(5);
+        t.printMethod2(5);
     }
 
-    public void print(int h) {
+    public void printMethod2(int h) {
+        for (int i = 1; i <= h; i++) {
+            for (int j = 1; j <= i; j++) {
+                if((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void printMethod1(int h) {
         boolean one = true;
         for (int i = 1; i <= h ; i++) {
             one = i%2==1;
