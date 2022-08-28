fa12d4bf5339c7c407280a1735a6de8afd5b3653
Rajanikant <kantrajni304@gmail.com>
2022-08-28 23:35:31 +0530

    array - search a key in array

---
+++ src/algorithms/LinearSearch.java
@@ -0,0 +1,26 @@
+package algorithms;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 5 ,6, 9, 10, 14, 16 ,19};
+        int key = 10;
+
+        LinearSearch ls = new LinearSearch();
+        int index = ls.indexOf(arr, key);
+
+        if(index == -1){
+            System.out.println("not found");
+        } else {
+            System.out.println("key found at index : " + index);
+        }
+    }
+
+    public int indexOf(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
