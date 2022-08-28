52b6b18a69d11c7e1970e7cb4c4809147d10a713
Rajanikant <kantrajni304@gmail.com>
2022-08-28 10:48:16 +0530

    loops - butterfly pattern second method

--- src/javaprogramming/loops/pattern/Butterfly.java
+++ src/javaprogramming/loops/pattern/Butterfly.java
@@ -3,10 +3,39 @@ package javaprogramming.loops.pattern;
 public class Butterfly {
     public static void main(String[] args) {
         Butterfly b = new Butterfly();
-        b.print(5);
+        b.printMethod1(5);
+        b.printMethod2(5);
     }
 
-    public void print(int h) {
+    public void printMethod2(int h) {
+        for (int i = 1; i <= h; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (h - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = h; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (h - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printMethod1(int h) {
         for (int i = 1; i <= h; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
