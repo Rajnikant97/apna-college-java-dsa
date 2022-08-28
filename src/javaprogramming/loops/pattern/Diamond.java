e661b3e3910c2c1fa0e19085da8d4ad3f57a9a64
Rajanikant <kantrajni304@gmail.com>
2022-08-28 12:43:31 +0530

    loops - diamond pattern second method

--- src/javaprogramming/loops/pattern/Diamond.java
+++ src/javaprogramming/loops/pattern/Diamond.java
@@ -4,10 +4,34 @@ public class Diamond {
 
     public static void main(String[] args) {
         Diamond d = new Diamond();
-        d.print(5);
+        d.printMethod1(5);
+        d.printMethod2(4);
     }
 
-    public void print(int h) {
+    public void printMethod2(int h) {
+        for (int i = 1; i <= h; i++) {
+            for (int j = 1; j <= h-i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i -1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = h; i >= 1; i--) {
+            for (int j = 1; j <= h-i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i -1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public void printMethod1(int h) {
         for (int i = 1; i <= h; i++) {
             for (int j = 1; j <= h-i; j++) {
                 System.out.print(" ");
