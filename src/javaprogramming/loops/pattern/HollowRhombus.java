8c30df2484a4a9736260a039a46195a9efc546e4
Rajanikant <kantrajni304@gmail.com>
2022-08-28 12:25:46 +0530

    loops - hollow rhombus pattern second method

--- src/javaprogramming/loops/pattern/HollowRhombus.java
+++ src/javaprogramming/loops/pattern/HollowRhombus.java
@@ -3,10 +3,28 @@ package javaprogramming.loops.pattern;
 public class HollowRhombus {
     public static void main(String[] args) {
         HollowRhombus hr = new HollowRhombus();
-        hr.print(5);
+        hr.printMethod1(5);
+        hr.printMethod2(7);
     }
 
-    public void print(int h) {
+    public void printMethod2(int h) {
+        for (int i = 1; i <= h; i++) {
+            for (int j = 1; j <= h-i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= h; j++) {
+                if(i == 1 || i == h || j == 1 || j == h) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void printMethod1(int h) {
         for (int i = 1; i <= h-1; i++) {
             System.out.print(" ");
         }
