534ad9b3904e74b46e64bb8c92894847431f93ec
Rajanikant <kantrajni304@gmail.com>
2022-08-28 09:44:52 +0530

    loops - hollow rectangle pattern with rows and columns

--- src/javaprogramming/loops/pattern/HollowRectangle.java
+++ src/javaprogramming/loops/pattern/HollowRectangle.java
@@ -4,8 +4,21 @@ public class HollowRectangle {
     public static void main(String[] args) {
         HollowRectangle hr = new HollowRectangle();
         hr.print(5);
+        hr.print(6,4);
     }
 
+    public void print(int rows, int cols) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if(i == 1 || i == rows || j == 1 || j == cols) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
     public void print(int h) {
 
         for (int i = 1; i <= h; i++) {
