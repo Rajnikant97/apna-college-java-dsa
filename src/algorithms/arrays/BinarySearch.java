f614902061d6851b57d83c6d8dc4893f7716a45f
Rajanikant <kantrajni304@gmail.com>
2022-08-30 23:38:15 +0530

    arrays - searching algorithms move to arrays package

--- src/algorithms/arrays/BinarySearch.java
+++ src/algorithms/arrays/BinarySearch.java
@@ -1,4 +1,4 @@
-package algorithms;
+package algorithms.arrays;
 
 public class BinarySearch {
     public static void main(String[] args) {
--- src/algorithms/arrays/LinearSearch.java
+++ src/algorithms/arrays/LinearSearch.java
@@ -1,4 +1,4 @@
-package algorithms;
+package algorithms.arrays;
 
 public class LinearSearch {
     public static void main(String[] args) {
