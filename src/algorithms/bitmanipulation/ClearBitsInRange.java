7dbb6685734bbaa29a25ec106b410b6d7990ba53
Rajanikant <kantrajni304@gmail.com>
2022-09-06 20:24:39 +0530

    bit manipulation - clear bits in given range method 2

--- src/algorithms/bitmanipulation/ClearBitsInRange.java
+++ src/algorithms/bitmanipulation/ClearBitsInRange.java
@@ -3,10 +3,17 @@ package algorithms.bitmanipulation;
 public class ClearBitsInRange {
     public static void main(String[] args) {
         int num = 95;
+        int least = 1;
+        int most = 3;
         GetIthBit.printBits(num, 8);
 
         System.out.println("clear bits in range");
-        num = clearBitsInRange(num, 1,3);
+        num = clearBitsInRange(num, least, most);
+        GetIthBit.printBits(num, 8);
+
+        num = 95;
+        System.out.println("clear bits in range 2");
+        num = clearBitsInRange2(num, least, most);
         GetIthBit.printBits(num, 8);
     }
 
@@ -20,5 +27,13 @@ public class ClearBitsInRange {
         GetIthBit.printBits(bitMask2, 8);
         GetIthBit.printBits(bitMask, 8);
         return num & bitMask;
+    }
+
+    public static int clearBitsInRange2(int num, int least, int most) {
+        for (int i = least; i <= most; i++) {
+            int bitMask  = ~(1 << i);
+            num = num & bitMask;
+        }
+        return num;
     }
 }
