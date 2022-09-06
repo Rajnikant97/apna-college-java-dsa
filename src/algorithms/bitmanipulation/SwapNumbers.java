00039f7eb3913c17b57928e143b377410afb7fa1
Rajanikant <kantrajni304@gmail.com>
2022-09-06 21:48:21 +0530

    bit manipulation - swap number using xor

---
+++ src/algorithms/bitmanipulation/SwapNumbers.java
@@ -0,0 +1,14 @@
+package algorithms.bitmanipulation;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("a = " + a + ", b = " + b);
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
