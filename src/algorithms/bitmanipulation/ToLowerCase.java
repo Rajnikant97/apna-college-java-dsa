3722b0d5f25ebcd5a3311cde338eaa9feded1bfe
Rajanikant <kantrajni304@gmail.com>
2022-09-06 21:49:44 +0530

    bit manipulation - to upper case and to lower case

---
+++ src/algorithms/bitmanipulation/ToLowerCase.java
@@ -0,0 +1,13 @@
+package algorithms.bitmanipulation;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        for (char ch = 'A'; ch <= 'Z' ; ch++) {
+            System.out.print((char) (ch | ' '));
+        }
+        System.out.println();
+        for (char ch = 'a'; ch <= 'z' ; ch++) {
+            System.out.print((char) (ch ^ ' '));
+        }
+    }
+}
