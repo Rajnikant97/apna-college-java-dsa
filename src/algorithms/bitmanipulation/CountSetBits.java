38a1b4823ae8d4ac753724576ebc682df88f7779
Rajanikant <kantrajni304@gmail.com>
2022-09-07 21:29:07 +0530

    bit manipulation - count set bits Brian Kernighan's Algorithm:

--- src/algorithms/bitmanipulation/CountSetBits.java
+++ src/algorithms/bitmanipulation/CountSetBits.java
@@ -5,6 +5,7 @@ public class CountSetBits {
         int n = 101;
         GetIthBit.printBits(n, 8);
         System.out.println(countSetBits(n));
+        System.out.println(countSetBitsBrianKernighanAlgorithm(n));
     }
 
     public static int countSetBits(int n) {
@@ -12,6 +13,17 @@ public class CountSetBits {
         while(n > 0) {
             count += n  & 1;
             n >>= 1;
+        }
+        return count;
+    }
+
+
+
+    public static int countSetBitsBrianKernighanAlgorithm(int n) {
+        int count = 0;
+        while(n > 0) {
+            n &= (n-1);
+            count++;
         }
         return count;
     }
