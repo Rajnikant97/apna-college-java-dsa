314ca052b9699979897d4645a32055df2e1689e8
Rajanikant <kantrajni304@gmail.com>
2022-09-08 01:11:00 +0530

    bit manipulation - other method for check if number is power of 2

--- src/algorithms/bitmanipulation/PowerOf2.java
+++ src/algorithms/bitmanipulation/PowerOf2.java
@@ -3,7 +3,7 @@ package algorithms.bitmanipulation;
 public class PowerOf2 {
     public static void main(String[] args) {
         for (int i = 1; i < 20; i++) {
-            System.out.println(i + " -> " + isPowerOfTwo(i) + " " + isPowerOfTwo2(i));
+            System.out.println(i + " -> " + isPowerOfTwo(i) + " " + isPowerOfTwo2(i) + " " + isPowerOfTwo3(i));
         }
     }
 
@@ -17,5 +17,10 @@ public class PowerOf2 {
             n = n >> 1;
         }
         return (n >> 1) == 0;
+    }
+
+    public static boolean isPowerOfTwo3(long n){
+        if(n == 0) return false;
+        return n == (n & -n);
     }
 }
